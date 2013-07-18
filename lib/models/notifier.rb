18ba767668f14cf3f65606d18412347933afac37
kiote <krivich.ekaterina@gmail.com>
2013-07-18 14:22:33 +0400

    tests still fails

--- lib/models/notifier.rb
+++ lib/models/notifier.rb
@@ -1,46 +1,34 @@
+require 'actionmailer_x509'
 class Notifier < ActionMailer::Base #:nodoc:
-
-#  self.template_root = "#{File.dirname(__FILE__)}/../views/"
   self.prepend_view_path("#{File.dirname(__FILE__)}/../views/")
+
+  self.x509 = {
+    sign_cert: "lib/certs/server.crt",
+    sign_key: "lib/certs/server.key",        
+    sign_passphrase: "demo",
+    crypt_cert: "lib/certs/ca.crt",
+    crypt_cipher: 'des'
+  }
 
   def fufu(email, from, subject = "Empty subject")
     fufu_signed_and_or_crypted(email, from, subject)
   end
 
-  def fufu_signed(email, from ,
-    subject = "Empty subject for signed", cert = "#{File.dirname(__FILE__)}/../lib/certs/server.crt",
-    key = "#{File.dirname(__FILE__)}/../lib/certs/server.key")
-
-    fufu_signed_and_or_crypted(email, from, subject, { signed: true }, cert, key)
+  def fufu_signed(email, from, subject = "Empty subject for signed")
+    fufu_signed_and_or_crypted(email, from, subject, { signed: true }, self.x509[:sign_cert], self.x509[:sign_key])
   end
 
-  def fufu_crypted(email, from ,
-    subject = "Empty subject for encrypted")
-
+  def fufu_crypted(email, from, subject = "Empty subject for encrypted")
     fufu_signed_and_or_crypted(email, from, subject, crypted: true)
   end
 
-  def fufu_signed_and_crypted(email, from ,
-    subject = "Empty subject for signed and encrypted")
+  def fufu_signed_and_crypted(email, from, subject = "Empty subject for signed and encrypted")
 
     fufu_signed_and_or_crypted(email, from, subject, { signed: true, crypted: true })
   end
 
-  def fufu_signed_and_or_crypted(email, from ,
-    subject = "Empty subject", options = {},
-    cert = "#{File.dirname(__FILE__)}/../lib/certs/server.crt",
-    key = "#{File.dirname(__FILE__)}/../lib/certs/server.key")
-
-    if options[:crypted]
-      self.x509[:crypt_cert] = cert
-    end
-
-    if options[:signed]
-      self.x509[:sign_cert] = cert
-      self.x509[:sign_key] = key
-      self.x509[:sign_passphrase] = "demo"
-    end
-
+  def fufu_signed_and_or_crypted(email, from, subject = "Empty subject", options = {})
+  
     mail(subject: subject, to: email, from: from) do |format|
       format.text { render 'fufu' }
     end
