fc65624b48ea49434d62275dfd0b5bbf679f8074
Jenua Boiko <jeyboy@bigmir.net>
2013-07-26 15:15:58 +0300

    clean and fix not coded mails passing

--- lib/actionmailer_x509/x509.rb
+++ lib/actionmailer_x509/x509.rb
@@ -21,7 +21,8 @@ module ActionMailerX509
     def decode(encrypted_text)
       pkcs7 = read(encrypted_text) rescue OpenSSL::PKCS7.new(encrypted_text)
       pkcs7.decrypt(@rsa_key, certificate)
-    rescue encrypted_text
+    rescue
+      encrypted_text
     end
 
     def sign(text)
@@ -45,47 +46,3 @@ module ActionMailerX509
       end
   end
 end
-
-
-#PAYPAL_CERT_PEM = File.read("paypal_cert.pem")
-#@paypal_cert = OpenSSL::X509::Certificate.new(PAYPAL_CERT_PEM)
-#
-#APP_CERT_PEM = File.read("app_cert.pem")
-#@app_cert = OpenSSL::X509::Certificate.new(APP_CERT_PEM)
-#
-#APP_KEY_PEM = File.read("app_key.pem")
-#@app_key = OpenSSL::PKey::RSA.new(APP_KEY_PEM, '')
-#
-#PAYPAL_KEY_PEM = File.read("paypal_key.pem")
-#@paypal_key = OpenSSL::PKey::RSA.new(PAYPAL_KEY_PEM, '')
-#
-#CERT_STORE = OpenSSL::X509::Store.new
-#CERT_STORE.add_cert(@app_cert)
-#
-#data = Hash.new
-#data['customer_id'] = '123456789'
-#data['customer_name'] = 'Mr Smith'
-#
-#def encrypt_for_paypal(values)
-#  data_name_values = values.map { |k, v| "#{k}=#{v}" }
-#
-#  signed_data = OpenSSL::PKCS7::sign(@app_cert, @app_key, data_name_values.join("\n"), [], OpenSSL::PKCS7::BINARY)
-#
-#  cypher = OpenSSL::Cipher::new("AES-128-CFB")
-#
-#  encrypted_data = OpenSSL::PKCS7::encrypt([@paypal_cert], signed_data.to_der, cypher, OpenSSL::PKCS7::BINARY)
-#
-#  encrypted_data.to_s #.gsub("\n", "")
-#end
-#
-#def decrypt_by_paypal(encrypted_data)
-#  received_encrypted_data = OpenSSL::PKCS7.new(encrypted_data)
-#
-#  received_signed_data = received_encrypted_data.decrypt(@paypal_key, @paypal_cert)
-#
-#  p7_received_signed_data = OpenSSL::PKCS7.new(received_signed_data)
-#
-#  p7_received_signed_data.verify(nil, CERT_STORE, nil, OpenSSL::PKCS7::NOVERIFY)
-#
-#  p7_received_signed_data.data
-#end
